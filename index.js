4e6fd56e9b9746bf67ea87edba87ec961ed74206
Ave Nurme <nurme.ave@gmail.com>
2021-11-16 17:32:35 +0200

    Create separate JS files for index.html and word-game.html

---
+++ index.js
@@ -0,0 +1,17 @@
+/* 
+  Fixing the mobile viewport problem with 'height: 100vh'
+  which does not apply correctly on mobile devices due to
+  the address bar on top of the screen.
+ */
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
